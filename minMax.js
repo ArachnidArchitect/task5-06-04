885d66617e2501f8b2850e39f0da738447b829d4
ArachnidArchitect <proffesionalworkingstuff@gmail.com>
2024-06-04 16:37:19 +0200

    task 5

---
+++ minMax.js
@@ -0,0 +1,11 @@
+let myArray = [2,5,7,10,50]
+function returnMax() {
+   console.log(Math.max(...myArray))
+}
+function returnMin() {
+    console.log(Math.min(...myArray))
+}
+returnMax()
+returnMin()
+
+// symbol iterator
