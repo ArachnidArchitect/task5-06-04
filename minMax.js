bbf8ee754f957771d9bdea88d332e31410a22eaf
ArachnidArchitect <proffesionalworkingstuff@gmail.com>
2024-06-05 10:07:28 +0200

    task5 with html

--- minMax.js
+++ minMax.js
@@ -1,11 +1,24 @@
+let array = document.querySelector('#arrayDisp')
+let display = document.querySelector('#display')
+let button1 = document.querySelector('#min')
+let button2 = document.querySelector('#max')
+
+
 let myArray = [2,5,7,10,50]
 function returnMax() {
    console.log(Math.max(...myArray))
+   display.innerText = 'Minimum value is: '+Math.max(...myArray)
 }
 function returnMin() {
     console.log(Math.min(...myArray))
+    display.innerText = 'Maximum value is: '+Math.min(...myArray)
 }
-returnMax()
-returnMin()
 
+
+
+
+array.innerText = myArray
+
+button1.addEventListener('click', returnMin)
+button2.addEventListener('click', returnMax)
 // symbol iterator
